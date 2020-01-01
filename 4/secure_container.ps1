c68c7b926be88e1001478510d62bf77ef5fb94d3
Chris Barton <57161+Christophoclese@users.noreply.github.com>
2020-01-01 11:29:39 -0800

    Lazy commit
    If this were something I were collaborating on, I'd probably hate me.

--- 4/secure_container.ps1
+++ 4/secure_container.ps1
@@ -10,17 +10,25 @@ Param(
 )
 
 [string]$InputString = '156218-652527'
-[int32]$LowerBound = ($InputString -Split '-')[0]
-[int32]$UpperBound = ($InputString -Split '-')[1]
+[int32]$LowerBound, [int32]$UpperBound = $InputString -Split '-'
 [string[]]$ValidPasswords = @()
 
 For ($Password = $LowerBound; $Password -le $UpperBound; $Password++) {
-    # must match regex (11|22|33|44|55|66|77|88|99|00)
+    #Write-Verbose "Current Password: $Password"
     If ($Password -match '(11|22|33|44|55|66|77|88|99|00)') {
-        $PasswordString = [string]$Password
-        Write-Verbose $PasswordString
+        #Write-Verbose "Found adjacent digits: $Password ($(($Matches.Values | select -Unique) -Join ','))"
+
+        $PasswordString = [string]$Password # We need a string for the next section so we can get its length and index into it
+        $PasswordPreviousValue = 0
         For ($Position = 0; $Position -lt $PasswordString.Length; $Position++) {
-            Write-Host $PasswordString[$Position]
+            $PasswordCurrentValue = $PasswordString[$Position]
+            If ($PasswordCurrentValue -lt $PasswordPreviousValue) {
+                continue
+            }
+            $PasswordPreviousValue = ($PasswordCurrentValue)
         }
+        $ValidPasswords += $Password
     }
 }
+
+$ValidPasswords
